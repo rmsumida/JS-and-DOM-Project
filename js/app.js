f7eeae835dfb7376267e5dbff4c989e6fe1e34e8
Ryan Sumida <ryan.sumida@gmail.com>
2022-06-14 22:40:45 -0700

    Replaced anchor tags in navbar buttons and used javascript scrollIntoView method

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@
 */
 
 const sectionList = document.querySelectorAll("section");  // Create a node list of all section elements
+const navbarList = document.getElementById("navbar-list");
 
 /**
  * End Global Variables
@@ -31,7 +32,13 @@ const sectionList = document.querySelectorAll("section");  // Create a node list
  * 
 */
 
-
+// Scroll to event target
+function scrollTo(evt) {
+    console.log(evt.target + " was clicked.")
+    document.querySelector("section#" + evt.target.id).scrollIntoView({
+        behavior: "smooth"
+    });
+};
 
 /**
  * End Helper Functions
@@ -39,36 +46,50 @@ const sectionList = document.querySelectorAll("section");  // Create a node list
  * 
 */
 
-// build the nav
-//  New fragment to store list items
+// Code to Build the Nav Menu
+// For loop will be used to create  li > a structure.
+// New fragment to store list items
 const navFragment = document.createDocumentFragment();
 
 // for loop to build the nav list based on the sections on the page
 for (let i = 0; i < sectionList.length; i++) {
-    const newItem = document.createElement("li");
-    const newLink = document.createElement("a");
+    let newItem = document.createElement("li");
+    let newBtn = document.createElement("button");
+    //let newLink = document.createElement("a");
     let sectionData = sectionList[i].getAttribute("data-nav"); // Get data attribute of section
     let sectionId = sectionList[i].id;  // Get id of section
     newItem.className = sectionId;  // Set the class name of the li tag to use the section id
-    newLink.className = "menu-link";  // Set the class name of the anchor tag to menu-link
-    newLink.href = "#" + sectionId;  // Set the href of anchor tag to use the section id
-    newLink.innerText = sectionData;  // Set the anchor text to use the data-nav attirbute value
-    newItem.appendChild(newLink);  // Append the anchor tag to the li tag
+    //newLink.className = "menu-link";  // Set the class name of the anchor tag to menu-link
+    newBtn.id = sectionId;
+    newBtn.className = " menu-link";
+    newBtn.setAttribute("type", "button");
+    //newLink.href = "#" + sectionId;  // Set the href of anchor tag to use the section id
+    //newLink.innerText = sectionData;  // Set the anchor text to use the data-nav attirbute value
+    newBtn.innerText = sectionData;
+    //newItem.appendChild(newLink);  // Append the anchor tag to the li tag
+    newItem.appendChild(newBtn);
     navFragment.appendChild(newItem);  // Append the li and anchor to the fragment
 }
 
-//  Append the nav fragment to the ul with id navbar-list
+// Append the nav fragment to the ul with id navbar-list
 document.querySelector("ul#navbar-list").appendChild(navFragment);
+// Add Event Listener to navbar-list
+document.querySelector("ul#navbar-list").addEventListener('click',scrollTo);
 
 
-// Add class "active" to section when near top of viewport
+
+
 
 /**
-Intersection Observer w/o inline function.
-This code no longer used in favor of code below with theh inline function
-This code will remain in the project file as a reference
-For observer target elements use the variable "sectionList" previously declared in the global variables section
+* Intersection Observer w/o inline function.
+* The following IO code is no longer used in favor of IO code in the next section
+* that includes an inline function. This code will remain in the project file as
+* a reference.
+* The variable "sectionList" that was previously declared in the global variables
+* section will be used as the observer targets.
+*/
 
+/**
 //  Create variable for the root element
 const sectionContainer = document.querySelector("section.section-container");
 
@@ -97,8 +118,9 @@ function changeActive (entries, observer) {
 
 
 /**
-Intersection Observer with inline function
-For observer target elements use the variable "sectionList" previously declared in the global variables section
+* Intersection Observer with inline function
+* The variable "sectionList" that was previously declared in the global variables
+* section will be used as the observer targets
 */
 
 //  Create variable for the root element
@@ -115,7 +137,8 @@ const sectionIoOptions = {
 const sectionIoObserver = new IntersectionObserver ((entries, observer) => {
     entries.forEach(entry => {
         entry.target.classList.toggle("active-section", entry.isIntersecting);
-        document.querySelector("li." + entry.target.id).classList.toggle("active-section", entry.isIntersecting);
+        //document.querySelector("li." + entry.target.id).classList.toggle("active-section", entry.isIntersecting);
+        document.querySelector(".menu-link#" + entry.target.id).classList.toggle("active-section", entry.isIntersecting);
     });
 },sectionIoOptions);
 
@@ -123,6 +146,9 @@ const sectionIoObserver = new IntersectionObserver ((entries, observer) => {
 sectionList.forEach(section => {
     sectionIoObserver.observe(section);
 });
+
+
+
 
 /**
  * End Main Functions
